8e81bb2bf981f7bde08cb71593bef738c587dfeb
Zak Strassberg <zakstrassberg@gmail.com>
2018-03-30 23:41:18 -0400

    Global layout injects fonts

---
+++ src/layouts/Global.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { injectGlobal } from 'emotion';
+
+injectGlobal`
+    @import url('https://fonts.googleapis.com/css?family=Crimson+Text:400,400i,700,700i|Vollkorn+SC:600');
+`;
+
+export default ({ children, ...props }) => (
+  <div id="global" {...props}>
+    {children}
+  </div>
+);
