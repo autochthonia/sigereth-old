7b095f6684a251e49c382a4b88fa9490221d151d
Zak Strassberg <zakstrassberg@gmail.com>
2018-04-01 14:59:54 -0400

    UI

--- src/pages/CharmEditor/index.jsx
+++ src/pages/CharmEditor/index.jsx
@@ -5,19 +5,32 @@ import Charm from '../../molecules/Charm';
 import Page from '../../organisms/Page';
 
 const CharmEditorWrapper = styled.div({});
+const Button = styled.button({
+  background: 'transparent',
+  border: '1px solid gold',
+  cursor: 'pointer',
+  borderRadius: 2,
+  margin: '0 auto',
+  '@media print': { display: 'none' },
+});
 
-const CharmEditor = ({ charms = [], editable, addCharm, onChange, toggleEditable }) => {
-  console.log(charms);
-  
-  return (
+const CharmEditor = ({ charms = [], editable, addCharm, onChange, toggleEditable }) => (
   <CharmEditorWrapper>
-    <Page header={<button onClick={toggleEditable}>Toggle edit</button>}>
+    <Page
+      header={
+        <Button onClick={toggleEditable}>
+          {editable ? 'Activate Markdown Mode' : 'Activate Edit Mode'}
+        </Button>
+      }
+    >
       {charms.map((c, idx) => (
         <Charm key={c.id || idx} {...c} editable={editable} onChange={payload => onChange({ ...c, ...payload }, idx)} />
       ))}
-      <button onClick={addCharm}>Add Charm</button>
+      <Button css={{ width: '100%' }} onClick={addCharm}>
+        Add Charm
+      </Button>
     </Page>
   </CharmEditorWrapper>
-)};
+);
 
 export default CharmEditor;
