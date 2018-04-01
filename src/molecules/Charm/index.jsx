43956b6e3bfd6f6a53ad1f72578d1f514d3a6dc9
Zak Strassberg <zakstrassberg@gmail.com>
2018-04-01 01:37:14 -0400

    charm gets editable fields

--- src/molecules/Charm/index.jsx
+++ src/molecules/Charm/index.jsx
@@ -1,9 +1,13 @@
+import { css } from 'emotion';
 import React from 'react';
 import Remarkable from 'remarkable';
 import RemarkableReactRenderer from 'remarkable-react';
 import styled from 'react-emotion';
 
-import { Body, BodyDiv, BodyP, H4 } from '../../styles/type';
+import { RIEInput, RIESelect, RIETextArea } from 'riek';
+
+import { BodyDiv, BodyP, H4 } from '../../styles/type';
+import { charmTypes } from '../../mocks/charm';
 import { mdBody } from '../../styles/markdown';
 
 const md = new Remarkable();
@@ -25,15 +29,17 @@ const CharmText = styled(BodyP)({
   },
 });
 
-const Charm = ({ name, cost, mins, type, keywords, duration, prereqs, text, editable }) => (
+const Charm = ({ name, cost, mins, type, keywords, duration, prereqs, text, editable, onChange }) => (
   <CharmWrapper>
-    <H4>{name}</H4>
+    {editable ? 'true' : 'false'}
+    <H4>{editable ? <RIEInput value={name} change={onChange} propName="name" /> : name}</H4>
     <BodyDiv>
-      <b>Cost:</b> {cost};&nbsp;
-      <b>Mins:</b> {mins}
+      <b>Cost:</b> {editable ? <RIEInput value={cost} change={onChange} propName="cost" /> : cost};&nbsp;
+      <b>Mins:</b> {editable ? <RIEInput value={mins} change={onChange} propName="mins" /> : mins}
     </BodyDiv>
     <BodyDiv>
-      <b>Type:</b> {type}
+      <b>Type:</b>{' '}
+      {editable ? <RIESelect value={type} change={onChange} propName="type" options={charmTypes} /> : type.text}
     </BodyDiv>
     <BodyDiv>
       <b>Keywords:</b> {keywords}
@@ -42,9 +48,9 @@ const Charm = ({ name, cost, mins, type, keywords, duration, prereqs, text, edit
       <b>Duration:</b> {duration}
     </BodyDiv>
     <BodyDiv>
-      <b>Prerequisite Charms:</b> {prereqs}
+      <b>Prerequisite Charms:</b> {editable ? <RIEInput value={prereqs} change={onChange} propName="prereqs" /> : prereqs}
     </BodyDiv>
-    {mdBody.render(text)}
+    {editable ? <RIETextArea rows="12" className={css({width: '100%'})} value={text} change={onChange} propName="text" /> : mdBody.render(text)}
   </CharmWrapper>
 );
 
