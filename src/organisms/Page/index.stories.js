dd372ce84309ad770061540c0e9e11cdaffdeea2
Zak Strassberg <zakstrassberg@gmail.com>
2018-04-01 00:18:31 -0400

    Tweak Page

--- src/organisms/Page/index.stories.js
+++ src/organisms/Page/index.stories.js
@@ -6,9 +6,19 @@ import { storiesOf } from '@storybook/react';
 import { wdcEvos } from '../../mocks/charm';
 import Charm from '../../molecules/Charm';
 import Page from '.';
+import QuickCharacter from '../../molecules/QuickCharacter';
+import { bloodApe } from '../../mocks/character';
 
-storiesOf('organisms/Page', module).add('tree', () => (
-  <div className={css({ display: 'flex', alignItems: 'center', justifyContent: 'center' })}>
-    <Page>{wdcEvos.map(c => <Charm {...c} key={c.name} />)}</Page>
-  </div>
-));
+storiesOf('organisms/Page', module)
+  .add('charm tree', () => (
+    <div className={css({ display: 'flex', alignItems: 'center', justifyContent: 'center' })}>
+      <Page>{wdcEvos.map(c => <Charm {...c} key={c.name} />)}</Page>
+    </div>
+  ))
+  .add('QC', () => (
+    <div className={css({ display: 'flex', alignItems: 'center', justifyContent: 'center' })}>
+      <Page>
+        <QuickCharacter {...bloodApe} />
+      </Page>
+    </div>
+  ));
