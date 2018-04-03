ab35c4e27c4004d6a2de2e1e8ce99b7d4acc5c9c
Zak Strassberg <zakstrassberg@gmail.com>
2018-04-02 23:59:50 -0400

    file-loader

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,5 +1,22 @@
 const rewireEmotion = require('react-app-rewire-emotion');
+const merge = require('lodash/merge');
 
 module.exports = function override(config, env) {
-  return rewireEmotion(config, env, { inline: true });
+  const newConfig = merge({}, config, {
+    module: {
+      rules: [
+        ...config.module.rules,
+        {
+          test: /\.(png|jpg|gif)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {},
+            },
+          ],
+        },
+      ],
+    },
+  });
+  return rewireEmotion(newConfig, env, { inline: true });
 };
