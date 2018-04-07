24fcfb2173f053530c5382e0983b7566b747ca3d
Zak Strassberg <zakstrassberg@gmail.com>
2018-04-07 00:16:25 -0400

    get fonts working

--- src/layouts/Global.jsx
+++ src/layouts/Global.jsx
@@ -3,27 +3,14 @@ import React from 'react';
 
 import watercolor from './seamless-watercolor-10.jpg';
 
-injectGlobal({
-  '@import':
-    "url('https://fonts.googleapis.com/css?family=Vollkorn+SC:600|Vollkorn:400|Crimson+Text:400,400i,700,700i')",
-  body: {
-    margin: 0,
-    ':before': {
-      content: '""',
-      position: 'fixed',
-      left: '0',
-      right: '0',
-      zIndex: '-1',
-      display: 'block',
-      backgroundImage: `url(${watercolor})`,
-      // backgroundSize: 'cover',
-      width: '100%',
-      height: '100%',
-      filter: 'blur(5px) opacity(0.4)',
+injectGlobal(
+  `@import url('https://fonts.googleapis.com/css?family=Vollkorn+SC:600|Vollkorn:400|Crimson+Text:400,400i,700,700i');`,
+  {
+    body: {
+      margin: 0,
     },
-  },
-});
-// Alike:400
+  }
+);
 
 export default ({ children, ...props }) => (
   <div id="global" css={{ overflow: 'auto' }} {...props}>
