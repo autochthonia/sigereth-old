d73d1d1177ab6b2bfe022f90a778f5fdde74c2dc
Zak Strassberg <zakstrassberg@gmail.com>
2018-04-09 00:09:12 -0400

    TextEditor

---
+++ src/organisms/TextEditor/index.jsx
@@ -0,0 +1,81 @@
+import { Editor } from 'slate-react';
+import { Value } from 'slate';
+import { css, cx } from 'emotion';
+import React, { Component } from 'react';
+import Plain from 'slate-plain-serializer';
+
+/**
+ * TODO:
+ * Select text on tab
+ * Put cursor at beginning of placeholder
+ */
+
+export default class TextEditor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.plain
+        ? Plain.deserialize(props.content)
+        : Value.fromJSON({
+            document: {
+              nodes: [
+                {
+                  object: 'block',
+                  type: props.multi ? 'paragraph' : 'span',
+                  nodes: [
+                    {
+                      object: 'text',
+                      leaves: [
+                        {
+                          text: props.content,
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          }),
+    };
+  }
+
+  _onKeyDown = (event, change) => {
+    if (event.key != 'Enter') return;
+    event.preventDefault();
+    change.blur();
+    return true;
+  };
+
+  _onChange = ({ value }) =>
+    this.setState({ value }, () => this.props.onChange(Plain.serialize(value), this.props.name));
+
+  _renderPlaceholder = props => {
+    const { node, editor } = props;
+    if (node.object != 'block') return;
+    // if (node.type != 'caption') return;
+    if (node.text != '') return;
+    return (
+      <span
+        contentEditable={false}
+        style={{ display: 'inline-block', whiteSpace: 'nowrap', opacity: '0.33', pointerEvents: 'none' }}
+      >
+        {editor.props.placeholder}
+      </span>
+    );
+  };
+
+  render() {
+    const { className, content, multi, el: Element, ...props } = this.props;
+    const { value } = this.state;
+    return (
+      <Editor
+        {...props}
+        className={cx(className, css({ display: multi ? 'block' : 'inline-block' }))}
+        value={value}
+        onChange={this._onChange}
+        renderPlaceholder={this._renderPlaceholder}
+        onKeyDown={this._onKeyDown}
+      />
+    );
+  }
+}
---
+++ src/organisms/TextEditor/index.stories.js
@@ -0,0 +1,21 @@
+import { withStateHandlers } from 'recompose';
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+import TextEditor from '.';
+
+storiesOf('organisms/TextEditor', module).add('charm tree', () => (
+  <div>
+    <h1>Single</h1>
+    <label>Single Label</label>
+    <TextEditor content="This is single line content"/>
+    <label>Single Label</label>
+    <TextEditor content="This is single line content too"/>
+    <h1>Multi</h1>
+    <label>Multi Label</label>
+    <TextEditor multi content="This is multiline content"/>
+    <label>Multi Label</label>
+    <TextEditor multi content="This is multiline content too"/>
+  </div>
+));
