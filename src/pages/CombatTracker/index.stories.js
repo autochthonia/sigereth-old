cafe7c7c55e0774cdaa973c392ef5caf28fdd1ff
Zak Strassberg <zakstrassberg@gmail.com>
2018-04-15 23:40:54 -0400

    CombatTracker

--- src/pages/CombatTracker/index.stories.js
+++ src/pages/CombatTracker/index.stories.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-import { storiesOf } from '@storybook/react';
+import { storiesOf, action } from '@storybook/react';
 
 import { defaultCharacter } from '../../mocks/character';
 import CombatTracker from '.';
 
-storiesOf('pages/CombatTracker', module)
-  .add('default', () => (
-    <CombatTracker name="foo" turn="0" combatants={[{...defaultCharacter, turnOver: false}, {...defaultCharacter, turnOver: true}]} />
-  ))
+storiesOf('pages/CombatTracker', module).add('default', () => (
+  <CombatTracker
+    name="foo"
+    turn="0"
+    combatants={[{ ...defaultCharacter, turnOver: false }, { ...defaultCharacter, turnOver: true }]}
+    mutateCombatant={action('mutateCombatant')}
+  />
+));
