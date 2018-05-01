0265e6fbc1f326a09d07b38ef3315e15e62881be
Zak Strassberg <zakstrassberg@gmail.com>
2018-05-01 00:09:25 -0400

    Combatant

--- src/organisms/Combatant/index.jsx
+++ src/organisms/Combatant/index.jsx
@@ -11,6 +11,7 @@ import Flex from '../../atoms/Flex';
 import InitEditor from './InitEditor';
 import Motes from './Motes';
 import NameEditor from './NameEditor';
+import NumberInput from '../../atoms/NumberInput';
 
 const CombatantWrapper = styled.div(({ turnOver }) => ({
   background: turnOver ? 'grey' : 'gold',
@@ -21,66 +22,69 @@ const Checkbox = styled.input({});
 const Combatant = ({
   mutate,
   className,
-  combatant: {
-    name: combatantName,
-    init,
-    turnOver,
-    personalMotes,
-    tempPersonalMotes,
-    peripheralMotes,
-    tempPeripheralMotes,
-    willpower,
-    tempWillpower,
-    character: {
-      name: characterName
-    }
-  },
-}) => (
-  <CombatantWrapper className={className} turnOver={turnOver}>
-    <Flex alignItems="center" css={{ padding: 6 }}>
-      <Checkbox checked={turnOver} type="checkbox" onClick={() => mutate(!turnOver, 'turnOver')} />
-      <InitEditor
-        number
-        init={init}
-        className={css({ ...h3CSS, marginLeft: 6, marginRight: 6 })}
-        onChange={mutate}
-        name="init"
-      />
-      <NameEditor
-        name={combatantName || characterName}
-        className={css({ ...h3CSS, display: 'inline-flex', alignContent: 'center' })}
-        onChange={mutate}
-      />
-      <section
-        css={{
-          paddingLeft: 12,
-          paddingRight: 12,
-          display: 'inline-flex',
-          justifyContent: 'space-around',
-          flex: '1 1 auto',
-        }}
-      >
-        <Motes type="Personal" temp={tempPersonalMotes} permanent={personalMotes} />
-        <Motes type="Peripheral" temp={tempPeripheralMotes} permanent={peripheralMotes} />
-        <Tooltip title="Willpower">
-          <span>{tempWillpower}</span>
-        </Tooltip>
-      </section>
-      <Clear css={{ cursor: 'pointer', ':hover': { fill: 'red' } }} />
-    </Flex>
-  </CombatantWrapper>
-);
+  combatant: { name: combatantName, init, turnOver, tempPersonalMotes, tempPeripheralMotes, tempWillpower, character },
+  removeCombatant,
+}) => {
+  const { personalMotes, peripheralMotes, willpower, name: characterName } = character || {};
+  return (
+    <CombatantWrapper className={className} turnOver={turnOver}>
+      <Flex alignItems="center" css={{ padding: 6 }}>
+        <Checkbox checked={turnOver} type="checkbox" onClick={() => mutate(!turnOver, 'turnOver')} />
+        <NumberInput
+          name="init"
+          value={init}
+          onChange={mutate}
+          className={css({ ...h3CSS, marginLeft: 6, marginRight: 6 })}
+        />
+        <NameEditor
+          name={combatantName || characterName}
+          className={css({ ...h3CSS, display: 'inline-flex', alignContent: 'center' })}
+          onChange={mutate}
+        />
+        <section
+          css={{
+            paddingLeft: 12,
+            paddingRight: 12,
+            display: 'inline-flex',
+            justifyContent: 'space-around',
+            flex: '1 1 auto',
+          }}
+        >
+          <Motes
+            type="Personal"
+            temp={tempPersonalMotes}
+            permanent={personalMotes}
+            onChange={mutate}
+            name="tempPersonalMotes"
+          />
+          <Motes
+            type="Peripheral"
+            temp={tempPeripheralMotes}
+            permanent={peripheralMotes}
+            onChange={mutate}
+            name="tempPeripheralMotes"
+          />
+          <Tooltip title="Willpower">
+            <NumberInput name="tempWillpower" value={tempWillpower} onChange={mutate} />
+            <span>/ {willpower}</span>
+          </Tooltip>
+        </section>
+        <Clear css={{ cursor: 'pointer', ':hover': { fill: 'red' } }} onClick={removeCombatant} />
+      </Flex>
+    </CombatantWrapper>
+  );
+};
 
 Combatant.fragments = {
   combatant: gql`
     fragment CombatantFields on Combatant {
       id
+      name
       init
       turnOver
       tempPersonalMotes
       tempPeripheralMotes
       tempWillpower
-      name
       character {
         id
         name
@@ -107,6 +111,7 @@ Combatant.propTypes = {
   }).isRequired,
   mutate: PropTypes.func,
   className: PropTypes.string,
+  removeCombatant: PropTypes.func.isRequired,
 };
 
 export default Combatant;
