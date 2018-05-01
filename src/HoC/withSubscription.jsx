d72b4971e2a9451f13e6f0026e981ffb030c7893
Zak Strassberg <zakstrassberg@gmail.com>
2018-05-01 01:12:55 -0400

    add withSubscription error if no props.subscribe

--- src/HoC/withSubscription.jsx
+++ src/HoC/withSubscription.jsx
@@ -5,6 +5,8 @@ export default WrappedComponent =>
     componentDidMount() {
       if (this.props.subscribe && typeof this.props.subscribe === 'function') {
         this.props.subscribe();
+      } else {
+        console.error('this.props.subscribe is missing or not a function')
       }
     },
   })(WrappedComponent);
