8e7b1ba348439553b8e00c7f1b966f2157f08a0d
Zak Strassberg <zakstrassberg@gmail.com>
2018-05-01 00:07:22 -0400

    add to importjs.js

--- .importjs.js
+++ .importjs.js
@@ -4,7 +4,7 @@ module.exports = {
   },
   namedExports: {
     react: ['Component'],
-    lodash: ['without', 'noop', 'debounce', 'orderBy', 'merge', 'isNumber', 'toNumber'],
+    lodash: ['without', 'noop', 'debounce', 'orderBy', 'merge', 'isNumber', 'toNumber', 'reject', 'uniqueId'],
     '@storybook/react': ['storiesOf', 'action'],
     'react-apollo': ['Mutation', 'Query'],
   },
