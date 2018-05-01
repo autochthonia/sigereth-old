7772aec30285f7d16a4bac3629cdf57a97658284
Zak Strassberg <zakstrassberg@gmail.com>
2018-05-01 00:08:06 -0400

    motes

--- src/organisms/Combatant/Motes.jsx
+++ src/organisms/Combatant/Motes.jsx
@@ -2,13 +2,15 @@ import { Tooltip } from 'react-tippy';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
+import NumberInput from '../../atoms/NumberInput';
+
 export default class Motes extends Component {
   render() {
     return (
       <Tooltip title={`${this.props.type} Motes`}>
         <span>
-          {this.props.temp}
-          {this.props.permanent !== undefined && `/${this.props.permanent}`}
+          <NumberInput onChange={this.props.onChange} value={this.props.temp} name={this.props.name} />
+          <span>{this.props.permanent !== undefined && `/${this.props.permanent}`}</span>
         </span>
       </Tooltip>
     );
@@ -19,4 +21,6 @@ Motes.propTypes = {
   type: PropTypes.oneOf(['Personal', 'Peripheral']).isRequired,
   temp: PropTypes.number.isRequired,
   permanent: PropTypes.number,
+  onChange: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
 };
