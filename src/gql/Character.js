72825bc8c5b453de21e2c35fa8ea3a2a8f12975c
Zak Strassberg <zakstrassberg@gmail.com>
2018-05-05 17:47:46 -0400

    gql

--- src/gql/Character.js
+++ src/gql/Character.js
@@ -1,6 +1,8 @@
-import { Query } from 'react-apollo';
+import { Mutation, Query } from 'react-apollo';
 import { gql } from 'apollo-boost';
 import React from 'react';
+
+import { GET_COMBATANT } from './Combatant';
 
 export const GET_CHARACTER = gql`
   query Character($id: ID!) {
@@ -24,4 +26,49 @@ export const GetAllCharacters = ({ children, ...props }) => (
   <Query {...props} query={GET_ALL_CHARACTERS}>
     {p => children(p)}
   </Query>
+);
+
+export const LINK_COMBATANT_TO_CHARACTER = gql`
+  mutation linkCharacterToCombatant($combatantId: ID!, $characterId: ID!) {
+    addToCombatantOnCharacter(combatantInstancesCombatantId: $combatantId, characterCharacterId: $characterId) {
+      characterCharacter {
+        id
+        name
+      }
+      combatantInstancesCombatant {
+        id
+      }
+    }
+  }
+`;
+
+export const MutationLinkCharacter = ({ children, ...props }) => (
+  <Mutation
+    mutation={LINK_COMBATANT_TO_CHARACTER}
+    update={(proxy, response) => {
+      console.log(response);
+
+      const {
+        data: {
+          addToCombatantOnCharacter: {
+            characterCharacter: character,
+            combatantInstancesCombatant: { id: combatantId },
+          },
+        },
+      } = response;
+
+      const { Combatant } = proxy.readQuery({
+        query: GET_COMBATANT,
+        variables: { id: combatantId },
+      });
+      proxy.writeQuery({
+        query: GET_COMBATANT,
+        variables: { id: combatantId },
+        data: { Combatant: { ...Combatant, character } },
+      });
+    }}
+    {...props}
+  >
+    {p => children(p)}
+  </Mutation>
 );
--- src/gql/Combat.js
+++ src/gql/Combat.js
@@ -2,18 +2,38 @@ import { Query } from 'react-apollo';
 import { gql } from 'apollo-boost';
 import React from 'react';
 
+import Combatant from '../organisms/Combatant';
+
+export const CombatFragments = gql`
+  fragment CombatFields on Combat {
+    id
+    turn
+    name
+    combatants {
+      id
+      init
+    }
+  }
+`;
+
 export const GET_COMBAT = gql`
   query combat($id: ID!) {
     Combat(id: $id) {
-      id
-      turn
-      name
-      combatants {
-        id
-        init
+      ...CombatFields
+    }
+  }
+  ${CombatFragments}
+`;
+
+export const COMBAT_SUBSCRIPTION = gql`
+  subscription($id: ID!) {
+    Combat(filter: { AND: [{ node: { id: $id } }, { mutation_in: UPDATED }] }) {
+      node {
+        ...CombatFields
       }
     }
   }
+  ${CombatFragments}
 `;
 
 export const GetCombat = ({ children, combatId, ...props }) => (
