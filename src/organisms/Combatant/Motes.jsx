65a9d41945c24a6bfd0474d679468c92f7e45edd
Zak Strassberg <zakstrassberg@gmail.com>
2018-05-06 23:41:58 -0400

    Motes

--- src/organisms/Combatant/Motes.jsx
+++ src/organisms/Combatant/Motes.jsx
@@ -5,12 +5,46 @@ import React, { Component } from 'react';
 import NumberInput from '../../atoms/NumberInput';
 
 export default class Motes extends Component {
+  state = {
+    ctrlKey: false,
+  };
+
+  componentWillMount() {
+    window.addEventListener('keydown', e => {
+      if (e.ctrlKey) {
+        this.setState({
+          ctrlKey: true,
+        });
+      }
+    });
+    window.addEventListener('keyup', e => {
+      if (e.ctrlKey) {
+        this.setState({
+          ctrlKey: false,
+        });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown');
+    window.removeEventListener('keyup');
+  }
+
+  _onChange = (newValue, name, e) => {
+    const currentlyOverMax = this.props.temp > this.props.permanent;
+    if (this.props.permanent && newValue > this.props.permanent && !currentlyOverMax && !this.state.ctrlKey) {
+      return this.props.onChange(this.props.permanent, name);
+    }
+    return this.props.onChange(newValue, name);
+  };
+
   render() {
     return (
       <Tooltip title={`${this.props.type} Motes`}>
         <span>
-          <NumberInput min="0" onChange={this.props.onChange} value={this.props.temp} name={this.props.name} />
-          <span>{this.props.permanent !== undefined && `/${this.props.permanent}`}</span>
+          <NumberInput min="0" onChange={this._onChange} value={this.props.temp} name={this.props.name} />
+          <span>{this.props.permanent && `/${this.props.permanent}`}</span>
         </span>
       </Tooltip>
     );
@@ -23,4 +57,8 @@ Motes.propTypes = {
   permanent: PropTypes.number,
   onChange: PropTypes.func.isRequired,
   name: PropTypes.string.isRequired,
+};
+
+Motes.defaultProps = {
+  permanent: null,
 };
